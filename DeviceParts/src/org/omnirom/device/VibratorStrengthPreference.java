010c35f1ab4dcc90825bf2f8b07e8f28dc05067d
Marko Man <darkobas@gmail.com>
2018-06-25 00:47:19 +0200

    andd vib strenght

--- DeviceParts/src/org/omnirom/device/VibratorStrengthPreference.java
+++ DeviceParts/src/org/omnirom/device/VibratorStrengthPreference.java
@@ -41,7 +41,7 @@ public class VibratorStrengthPreference extends Preference implements
     private int mMaxValue;
     private Vibrator mVibrator;
 
-    private static final String FILE_LEVEL = "/sys/class/leds/vibrator/vmax_mv_user";
+    private static final String FILE_LEVEL = "/sys/devices/virtual/timed_output/vibrator/vtg_level";
     private static final long testVibrationPattern[] = {0,250};
 
     public VibratorStrengthPreference(Context context, AttributeSet attrs) {
@@ -50,7 +50,7 @@ public class VibratorStrengthPreference extends Preference implements
         // #define QPNP_HAP_VMAX_MIN_MV		116
         // #define QPNP_HAP_VMAX_MAX_MV		3596
         mMinValue = 116;
-        mMaxValue = 2088;
+        mMaxValue = 3596;
 
         mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
         setLayoutResource(R.layout.preference_seek_bar);
