f6d2c8b36fdbfca300144f48a969de86ef4612a7
Vachounet <vachounet@live.fr>
2019-08-24 20:02:56 +0200

    nx609j: sepolicy disaster
    Change-Id: Id1ee9138b0d449492546abf1babae0e646e2841c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -196,9 +196,8 @@ TARGET_SYSTEM_AUDIO_EFFECTS := true
 
 # selinux
 include device/qcom/sepolicy/sepolicy.mk
+BOARD_SEPOLICY_DIRS += $(BOARD_PATH)/sepolicy
 include vendor/omni/sepolicy/sepolicy.mk
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(BOARD_PATH)/sepolicy/public
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(BOARD_PATH)/sepolicy/private
 
 BOARD_SECCOMP_POLICY += $(BOARD_PATH)/seccomp_policy
 
